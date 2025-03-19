08894f7f38ece3aa93a3f6d4a077f60e081de2d1
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2025-03-19 14:57:58 +0300

    dropped nested `for` loops

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,41 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - prints all possible combinations of two-two digit numbers
  * Return: 0
  */
 int main(void)
 {
+	int num = 0;
 	int num1, num2;
 
-	for (num1 = 0 ; num1 <= 99 ; num1++)
+	while (num < 10000)
 	{
-		for (num2 = 0 ; num2 <= 99 ; num2++)
+		num1 = num / 100;
+		num2 = num % 100;
+
+		if (num1 < num2)
 		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (num1 <= num2)
+			if (!(num1 == 98 && num2 == 99))
 			{
-
-				putchar(num1 / 10 + '0');
-				putchar(num1 % 10 + '0');
-
+				putchar(',');
 				putchar(' ');
-
-				putchar(num2 / 10 + '0');
-				putchar(num2 % 10 + '0');
-
-				if (num1 != 99 || num2 != 99)
-				{
-
-					putchar(',');
-					putchar(' ');
-
-				}
 			}
 		}
+		num++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
