bf2029ee1b7933204b57e41a70b0ef11ca85c8cb
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2025-03-24 15:05:43 +0300

    use nested `while` instead of `for`

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,22 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - prints the alphabets 10x
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char a;
+	int i = 0;
+	int a;
 
-	for (i = 1 ; i <= 10 ; i++)
+	while (i < 10)
 	{
-		for (a = 'a' ; a <= 'z' ; a++)
+		a = 'a';
+		while (a <= 'z')
 		{
 			_putchar(a);
+			a++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
