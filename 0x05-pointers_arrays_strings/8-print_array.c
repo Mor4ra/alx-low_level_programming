c9813fdd43b7be64b680aa946a36aff351494a15
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2025-04-09 13:23:53 +0300

    added a function that prints the contents of an `int` array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,15 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
-	
-	for (i = 0 ; i < n ; i++)
+	int i = 0;
+
+	while (i < n)
 	{
-		printf("%d", a[i]);
-		
-		if (i < (n - 1))
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+		i++;
 	}
-	_putchar('\n');
-} 
+	printf("\n");
+}
