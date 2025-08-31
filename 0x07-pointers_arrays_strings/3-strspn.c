cffdd277b648135d514e518435f7cd4319e24879
Mor4ra <morarajoshua36@gmail.com>
2025-08-31 16:00:39 +0300

    This is my custom `strspn()`

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,47 @@
 #include "main.h"
-/**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+
+/* _strspn - calculates the length of the maximum initial
+ * segment of the string 's' that consists entirely of
+ * characters contained in the string 'accept'.
+ *
+ * [scanning stops at the first character in 's' that is not
+ * present in 'accept']
+ * @s: the string the function scans while comparing it
+ * to 'accept'.
+ * @accept: characters to be counted in 's' have to be here
+ * Return: the number of the first consecutive characters of
+ * 's' in 'accept'.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-        unsigned int n = 0;
-        int r;
-        while (*s)
-        {
-                for (r = 0; accept[r]; r++)
-                {
-                        if (*s == accept[r])
-                        {
-                                n++;
-                                break;
-                        }
-                        else if (accept[r + 1] == '\0')
-                                return (n);
-                }
-                s++;
-        }
-        return (n);
-}
+	unsigned int count = 0;
+	int i = 0, j, found;
 
+	while (s[i] != '\0')
+	{
+		j = 0;
+		found = FALSE;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				found = TRUE;
+				break;
+			}
+			j++;
+		}
+		if (found == TRUE)
+			count = count + 1;
+		else
+		{
+			/**
+			 * first character of 's' not in
+			 * 'accept', stop scanning.
+			 */
+			return (count);
+		}
+		i++;
+	}
+
+	return (count);
+}
