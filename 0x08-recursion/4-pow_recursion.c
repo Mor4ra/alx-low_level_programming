a1a8a63edf48baa50580e73ea382759e613a1353
Mor4ra <morarajoshua36@gmail.com>
2025-09-07 17:51:42 +0300

    Added a custom `pow()` function.

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _pow_recursion - calculates the value of; x^y
- * @x: the base
- * @y: the power
- * Return: 'x' raised to the power of 'y'
+ * _pow_recursion - recursively calculates `x^y`.
+ * @x: the base.
+ * @y: the power.
+ * Return: `x` raised to `y`.
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	if (y == 1)
+		return (x);
+	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, (y - 1)));
+	return (x * _pow_recursion(x, y - 1));
 }
