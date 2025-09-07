e813f783b93e0b162c86307fbda6959558e4b058
Mor4ra <morarajoshua36@gmail.com>
2025-09-07 17:26:36 +0300

    Added a custom `strlen()` function.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: pointer to the string whose length we're finding
- * Return: length of the string
+ * _strlen_recursion - recursively calculates the length of `s`.
+ * @s: the string.
+ * Return: the length.
  */
 int _strlen_recursion(char *s)
 {
-
 	if (*s == '\0')
 		return (0);
-
 	return (1 + _strlen_recursion(s + 1));
 }
