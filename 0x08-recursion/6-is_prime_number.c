a8d2d856dd1f0c6d8c2279dd8c4d7da25f71bb4d
Mor4ra <morarajoshua36@gmail.com>
2025-09-09 09:39:05 +0300

    Added `6-is_prime_number.c()` for recursive prime checking.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * is_divisible - checks if 'n' is divisible by a given divisor 'd'
- * @n: the number we're checking its divisiblity with 'd'
- * @d: the divisor
- * Return: 1 if any divisor exists that evenly divides 'n'
- * otherwise return 0.
+ * _prime_helper - recursively checks if a number is prime.
+ * @i: possible factor of the number.
+ * @n: the number to check it`s primality.
+ * Return: 1 if prime, 0 otherwise.
  */
-int is_divisible(int n, int d)
+int _prime_helper(int i, int n)
 {
-	if (d <= 1)
-		return (0);
-	if (n % d == 0)
+	if (i * i > n)
 		return (1);
-	else
-		return (is_divisible(n, d - 1));
+	if (n % i == 0)
+		return (0);
+	return (_prime_helper(i + 1, n));
 }
 
 /**
- * is_prime_number - checks 'primality'
- * @n: the number we're checking for 'primality'
- * Return: 1 if 'n' is prime, else return 0
+ * is_prime_number - checks if a number is prime.
+ * @n: the number to check it's primality.
+ * Return: 1 if prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	int i = 2;
+
+	if (n <= 1 || n % 2 == 0)
 		return (0);
-	return (!is_divisible(n, n - 1));
+	return (_prime_helper(i, n));
 }
