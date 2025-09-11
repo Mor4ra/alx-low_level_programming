e235d82fbbdb2723b2de8a83e063fb8cffa7287c
Mor4ra <morarajoshua36@gmail.com>
2025-09-11 14:25:28 +0300

    Added `101-wildcmp.c` which implements recursive wildcard matching with `*` support.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,36 +1,38 @@
 #include "main.h"
 
 /**
- * wildcmp_recursive - compares if two strings are identical
- * @s1: pointer to the 1st string we're comparing with another
- * @s2: pointer to the 2nd string
- * Return: 1 if 's1' and 's2' can be considered identical, otherwise
- * return 0.
- */
-int wildcmp_recursive(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s2 == '*')
-	{
-		int r1 = wildcmp_recursive(s1, s2 + 1);
-
-		return (r1 || (*s1 != '\0' && wildcmp_recursive(s1 + 1, s2)));
-	}
-
-	if (*s1 == *s2)
-		return (wildcmp_recursive(s1 + 1, s2 + 1));
-	return (0);
-}
-
-/**
- * wildcmp - checks if two strings can be considered identical
- * @s1: pointer to the first string
- * @s2: pointer to the 2nd string
- * Return: 1 if the strings are considered identical and 0 otherwise
+ * wildcmp - checks if two strings can be considered identical.
+ * @s1: the first string.
+ * @s2: the second string.
+ * Return: 1 if the two strings match, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (wildcmp_recursive(s1, s2) ? 1 : 0);
-}
+	/* base cases */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '\0' && *s1 != '\0')
+		return (0);
+	if (*s1 == '\0')
+	{
+		/* only match if remaining s2 is all `*`s */
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (0);
+	}
 
+	if (*s2 == '*')
+	{
+		/* 2 options */
+		return ((wildcmp(s1, s2 + 1)) || (wildcmp(s1 + 1, s2)));
+	}
+	else if (*s2 == *s1)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
